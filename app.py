b84c72f9b37f3aa446e876ab5a4fa2b2ce3238ca
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2020-12-28 16:47:37 +0100

    creating a REST API: step 1

--- app.py
+++ app.py
@@ -2,9 +2,45 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'My first web app'
+stores = [
+    {
+        'name': 'Feltrinelli',
+        'items': [
+            {
+                'name': 'Titolo 1',
+                'price': 10.00
+            }
+        ]
+    }
+]
+
+# POST: used to receive data
+# GET: used to send data back only
+
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'] )
+def create_store():
+    pass
+
+# GET  /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+# GET  /store
+@app.route('/store')
+def get_stores():
+    pass
+
+# POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item', methods=['POST'] )
+def create_item_in_store(name):
+    pass
+
+# GET  /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+    pass
 
 app.run(port=5000)
 
