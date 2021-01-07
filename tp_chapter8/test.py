9ff63be51592c2c452c8cf19e97a1d1988a9a2b1
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-01-07 17:17:08 +0100

    add more functions

--- tp_chapter8/test.py
+++ tp_chapter8/test.py
@@ -1,5 +1,6 @@
 import os
 
+
 def over_20_chr():
     fin = open("C:/Users/Lorenzo/Desktop/Informatica/tomorrowDevs/learn_python/python_exercises/tp_chapter8/words.txt")
     for line in fin:
@@ -15,4 +16,28 @@ def has_no_e():
             if c == 'e':
                 print(word)
 
-has_no_e()
+def avoids(word, unusable):
+    for letter in word:
+        if letter in unusable:
+            return False
+    return True
+
+def uses_only(word, usable):
+    for letter in word:
+        if letter not in usable:
+            return False
+    return True
+
+def uses_all(word, to_use):
+    for letter in to_use:
+        if letter not in word:
+            return False
+    return True
+
+def uses_all1(word, to_use):
+    return uses_only(to_use, word)
+
+ print()
+
+
+
