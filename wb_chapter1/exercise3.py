81fba53d503a4cc17af66e424c0202692213cef7
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-01-11 16:33:28 +0100

    changes to input strings

--- wb_chapter1/exercise3.py
+++ wb_chapter1/exercise3.py
@@ -1,9 +1,5 @@
-measure_unit = input('Choose which unit you prefer, feet or meters')
-if measure_unit == ' meters':
-    measure_unit = 'meters'
-if measure_unit == ' feet':
-    measure_unit = 'feet'
-length = float(input('Insert the room lenght'))
-width = float(input('Insert the room width'))
+measure_unit = input('Choose which unit you prefer, feet or meters: ')
+length = float(input('Insert the room lenght: '))
+width = float(input('Insert the room width: '))
 area = length * width
 print('The room area is %.2f %s' % (area, measure_unit))
