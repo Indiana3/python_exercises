875839dde71f16354004dfbd8f90246e444e03d5
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-01-11 11:03:37 +0100

    ex_1 execution

---
+++ wb_chapter1/exercise1.py
@@ -0,0 +1,9 @@
+first = "Lorenzo"
+last = "Muzzi"
+name = first + " " + last
+adress = "Via Cadorna 10"
+postal_code = 60123
+city = "Ancona"
+print ("%s" % name)
+print ("%s" % adress)
+print ("%i %s" % (postal_code, city))
