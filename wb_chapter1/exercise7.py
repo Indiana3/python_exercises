3b18da5a2ac13b44860b5d8eae1bf7db42c3ea4c
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-01-12 10:59:45 +0100

    ex_7 execution

---
+++ wb_chapter1/exercise7.py
@@ -0,0 +1,13 @@
+##
+# Compute the sum of the first n positive integers
+#
+
+# Take the number from user
+n = int(input('Enter a positive integer: '))
+
+# Compute the sum
+sm = n * (n+1) / 2
+
+# Display the output
+print("The sum of the first", n, "positive integers is %i" % sm)
+
