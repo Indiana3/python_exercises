24beb2a9d9aec7186d238638bc9bbf1a7ec421b6
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-01-18 17:50:05 +0100

    ex_26 execution

---
+++ wb_chapter1/exercise26.py
@@ -0,0 +1,10 @@
+##
+# Display current date and time
+#
+from time import asctime
+
+# Compute the current time and date
+t = asctime()
+
+# Display the current date and time
+print("The current date and time are", t)
