d6dc6db6981a202aead54c35f1422bdcb2d83a63
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-01-30 16:05:21 +0100

    ex_42 corrected

--- wb_chapter2/exercise42.py
+++ wb_chapter2/exercise42.py
@@ -1,28 +1,39 @@
 ##
 # Display the frequency of a note C0 to C8
 #
+C4_FREQ = 261.63
+D4_FREQ = 293.66
+E4_FREQ = 329.63
+F4_FREQ = 349.23
+G4_FREQ = 392.00
+A4_FREQ = 440.00
+B4_FREQ = 493.88
 
 # Read the note and the octave from the user
-note = input("Please, enter the note: ")
-octave = int(input("Please, enter the octave: "))
+name = input("Please, enter the two characters note name (e.g. C4): ")
 
-# Compute the corresponding frequency in Hz
+# Store the note and the octave in separable variables
+note = name[0]
+octave = int(name[1])
+
+# Get the frequency of the note in Hz, assuming it is in the fourth octave
 if note == "C":
-    hz = 261.63
+    freq = C4_FREQ
 elif note == "D":
-    hz = 293.66
+    freq = D4_FREQ
 elif note == "E":
-    hz = 329.63
+    freq = E4_FREQ
 elif note == "F":
-    hz = 349.23
+    freq = F4_FREQ
 elif note == "G":
-    hz = 392.00
+    freq = G4_FREQ
 elif note == "A":
-    hz = 440.00
+    freq = A4_FREQ
 elif note == "B":
-    hz = 493.88
+    freq = B4_FREQ
 
-hz = hz / 2 ** (4 - octave)
+# Adjust the frequency according to the octave
+freq = freq / 2 ** (4 - octave)
 
 # Display the result
-print("The frequency of", note, octave, "is %.2f" % hz)
+print("The frequency of", name, "is %.2f Hz" % freq)
