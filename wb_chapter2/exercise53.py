fffb34c866b6a68ee3c5a8ccf102b396275d9a2c
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-02-04 12:03:30 +0100

    ex_53 execution

--- wb_chapter2/exercise53.py
+++ wb_chapter2/exercise53.py
@@ -17,3 +17,44 @@ F = 0.0
 points = float(input("Please, enter your grade points: "))
 
 # Convert grade points into letter grade
+if abs (points - F) >= abs(A - F):
+    letter = "A+"
+else:
+    if abs(A - points) < abs(A_MINUS - points):
+        letter = "A"
+    else:
+        if abs(A_MINUS - points) < abs(B_PLUS - points):
+            letter = "A-"
+        else:
+            if abs(B_PLUS - points) < abs(B - points):
+                letter = "B+"
+            else:
+                if abs(B - points) < abs(B_MINUS - points):
+                    letter = "B"
+                else:
+                    if abs(B_MINUS - points) < abs(C_PLUS - points):
+                        letter = "B-"
+                    else:
+                        if abs(B_MINUS - points) < abs(C_PLUS - points):
+                            letter = "B-"
+                        else:
+                            if abs(C_PLUS - points) < abs(C - points):
+                                letter = "C+"
+                            else:
+                                if abs(C - points) < abs(C_MINUS - points):
+                                    letter = "C"
+                                else:
+                                    if abs(C_MINUS - points) < abs(D_PLUS - points):
+                                        letter = "C-"
+                                    else:
+                                        if abs(D_PLUS - points) < abs(D - points):
+                                            letter = "D+"
+                                        else:
+                                            if abs(D - points) < abs(F - points):
+                                                letter = "D"
+                                            else:
+                                                letter = "F"
+
+# Display the letter grade
+print(points, "grade points are equal to", letter)
+
