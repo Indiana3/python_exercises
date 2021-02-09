bb14a4e72f1d18a068011429ba6ffcd1f308d613
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-02-09 18:05:03 +0100

    ex_68 execution

---
+++ wb_chapter3/exercise64.py
@@ -0,0 +1,22 @@
+##
+# Display original prices, discount amounts and discounted prizes
+#
+DISCOUNT_RATIO = 0.6
+
+# Start with the lower prize
+prize = 4.95
+
+# For each product shows:
+# its original prize
+# the discount amount
+# the discounted prize
+while prize <= 24.95:
+    print()
+    print("Original prize: %.2f" % prize)
+    disc_amount = prize * DISCOUNT_RATIO
+    print("Discount amount: %.2f" % disc_amount)
+    disc_prize = prize - disc_amount
+    print("Discounted prize: %.2f" % disc_prize)
+    prize = prize + 5.00
+    
+
