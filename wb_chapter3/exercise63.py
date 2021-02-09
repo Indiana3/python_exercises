85a12829bdc30352c8b60e03b5d6f9d2aecef8fc
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-02-09 17:37:09 +0100

    ex_63 execution

---
+++ wb_chapter3/exercise63.py
@@ -0,0 +1,26 @@
+##
+# Computes the avarage of a collection of values
+#
+
+# Reads the first value from the user
+value = float(input("Please, enter the first value: "))
+
+# Checks the first value is not 0
+if value == 0:
+    print("The first value can't be 0")
+    avarage = None
+# Initialize the variables needed to compute the avarage of values
+else:
+    total = 0
+    n_value = 0
+
+# Computes and display the avarage of values 
+while value != 0:
+    total = total + value
+    n_value = n_value + 1
+    avarage = total / n_value
+    value = float(input("Please, enter the next value: "))
+
+# Display the avarange
+if avarage != None:
+    print("The avarage of this collection of values is", avarage)
