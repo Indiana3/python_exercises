7dfb23a410db025a6dc000a633484a40e37e05a4
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-02-10 13:09:29 +0100

    ex_66 started

---
+++ wb_chapter3/exercise66.py
@@ -0,0 +1,10 @@
+##
+# Display the total cost of n products, rounded to the
+# nearest nickel if customer pays with cash
+#
+NICKEL = 5
+
+# Read price from user
+price = float(input("Please, enter the price: "))
+while price != "":
+    pass
