2886fe81394476df6ff76e7c670d43123fd98188
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-02-17 12:11:50 +0100

    ex_77 execution

---
+++ wb_chapter3/exercise77.py
@@ -0,0 +1,18 @@
+##
+# Multiplication Table from 1 times 1 up to and
+# including 10 times 10
+#
+
+# Print 2 margin spacings for the row of labels
+# Print the row of labels
+print("  ", end="")
+for i in range(1, 11):
+    print("%4i" % i, end="")
+print()
+
+# Print the left side labels and all the multiplications
+for i in range(1, 11):
+    print("%2i" % i, end="")
+    for j in range(1, 11):
+        print("%4i" % (j * i), end="")
+    print()
