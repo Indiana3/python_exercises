7d2af268951511003e470401cf6ad16702b6d466
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-03-09 11:15:14 +0100

    ex_7 execution

---
+++ tp_chapter10/exercise7.py
@@ -0,0 +1,15 @@
+## Check if a list has duplicates of an element
+# @param t a list
+# @return True if t has duplicates of an element; False otherwise
+def has_duplicates(t):
+    t1 = []
+    for i in range (0, len(t)):
+        if t[i] in t1:
+            print(t1)
+            return True
+        t1.append(t[i])
+    print(t1)
+    return False
+
+# Call has_duplicates function and display the result
+print(has_duplicates([1, 2, 3, 1]))
