ee39cd1441ca0653d1c04c0f551f331a888bf93e
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-03-17 12:48:15 +0100

    ex_11 started

---
+++ tp_chapter10/exercise11.py
@@ -0,0 +1,22 @@
+##
+# Check how many reverse pairs there are in a word list
+#
+# from exercise9 import list_builder
+# from bisect import bisect_left
+
+# Import the list of word to check
+# t = list_builder('../tp_chapter8/words.txt')
+# print(t[:50])
+
+# for i in range(len(t)):
+#     word = t[i]
+#     reversed_word = word[::-1]
+#     j = bisect_left(t, reversed_word)
+#     if j != len(t) and reversed_word == t[j]:
+#         print(word)
+#         print(reversed_word)
+
+
+
+
+
