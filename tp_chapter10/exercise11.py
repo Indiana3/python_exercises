556b594d8cec889cf068966fe13bbeac3c2371c2
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-03-18 12:28:24 +0100

    ex_11 execution

--- tp_chapter10/exercise11.py
+++ tp_chapter10/exercise11.py
@@ -1,20 +1,27 @@
 ##
 # Check how many reverse pairs there are in a word list
 #
-# from exercise9 import list_builder
-# from bisect import bisect_left
+from exercise9 import list_builder
+from bisect import bisect_left
 
-# Import the list of word to check
-# t = list_builder('../tp_chapter8/words.txt')
-# print(t[:50])
+def main():
+    # Import the list of word to check
+    t = list_builder('../tp_chapter8/words.txt')
 
-# for i in range(len(t)):
-#     word = t[i]
-#     reversed_word = word[::-1]
-#     j = bisect_left(t, reversed_word)
-#     if j != len(t) and reversed_word == t[j]:
-#         print(word)
-#         print(reversed_word)
+    # For each string element, compute its reverse
+    for i in range(len(t)):
+        word = t[i]
+        reversed_word = word[::-1]
+    # Check if the reversed string is in the list t
+    # If it is, print the string element and the reversed string
+        j = bisect_left(t, reversed_word)
+        if j != len(t) and reversed_word == t[j]:
+            print(word)
+            print(reversed_word)
+
+# Call the main function
+if __name__ == "__main__":
+    main()
 
 
 
