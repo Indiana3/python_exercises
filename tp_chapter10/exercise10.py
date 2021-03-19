5d2b9eb8ef2f3086844745db9e7c4f3fab760563
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-03-19 18:44:52 +0100

    [REFACTORING] creation of a main function to run the program

--- tp_chapter10/exercise10.py
+++ tp_chapter10/exercise10.py
@@ -1,14 +1,11 @@
 from exercise9 import list_builder
 
-# Generate a list of word
-t = list_builder('../tp_chapter8/words.txt')
-
 ## Check if a word is in the list
 # @param t a sorted list
-# @param value the word we wnat to check
+# @param value the word we want to check
 # @param n the max number of times you have to cut the list before returning
 # @return True if the word is the list; False otherwise
-def in_bisect(t, value, n):
+def in_bisect(t, value, n = 17):
     half = len(t) // 2
     for _ in range (n):
         if value == t[half]:
@@ -17,8 +14,15 @@ def in_bisect(t, value, n):
             t = t[:half]
         elif value > t[half]:
             t = t[half:]
-        half = half // 2
+        half = len(t) // 2
     return False
 
 # Call in_bisect function and print the risult
-print(in_bisect(t, "abatis", 17))
+def main():
+    # Generate a list of word
+    t = list_builder('../tp_chapter8/words.txt')
+    print(in_bisect(t, "abatis"))
+
+# Call the main function
+if __name__ == "__main__":
+    main()
