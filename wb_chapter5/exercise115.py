2595661d3fac1aee09aa7be75420489a76e03de2
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-03-24 10:25:17 +0100

    ex_115 execution

---
+++ wb_chapter5/exercise115.py
@@ -0,0 +1,25 @@
+##
+# Read a positive integer and compute all its proper divisors
+#
+
+## Find the proper divisors of a positive integer
+# @param n a positive integer
+# @return a list with all the positive integers
+def proper_divisors(n):
+    divisors = []
+    for i in range (1, n+1):
+        if n % i == 0:
+            divisors.append(i)
+    return divisors
+
+# Read a positive integer from user and display its proper divisors
+def main():
+    num = int(input("Please, enter a positive integer: "))
+    if num <= 0:
+        print("You didn't enter a positive number")
+    else:
+        print("The proper divisors of {} are: {}".format(num, proper_divisors(num))) 
+
+# Call the main function
+if __name__ == "__main__":
+    main()
