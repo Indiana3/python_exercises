47bc3501a725b36dc227672b63b9c8812335a202
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-04-13 13:09:11 +0200

    [REF] changing name and implementation of precedence()

--- wb_chapter4/exercise97.py
+++ wb_chapter4/exercise97.py
@@ -6,13 +6,15 @@
 # @param s the operator string
 # @return an integer representing the precedence of a mathematical operator
 #
-def precedence(s):
-    if s == "+" or s == "-":
+def operatorPrecedence(operator):
+    if operator == "+" or operator == "-":
         return 1
-    if s == "*" or s == "/":
+    if operator == "*" or operator == "/":
         return 2
-    if s == "^":
-        return 3
+    if operator == "u+" or operator == "u-":
+        return 3 
+    if operator == "^":
+        return 4
     else:
         return -1
 
@@ -21,10 +23,10 @@ def main():
     s = input("Please, enter a mathematical operator: ")
     # Display the precedence according to the integer returned
     # by precedence function
-    if precedence(s) == -1:
+    if operatorPrecedence(s) == -1:
         print("You didn't enter a mathematical operator")
     else:
-        print("The operator {} has precedence {}".format(s, precedence(s)))
+        print("The operator {} has precedence {}".format(s, operatorPrecedence(s)))
 
 # Call the main function
 if __name__ == "__main__":
