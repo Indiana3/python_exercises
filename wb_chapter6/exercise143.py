50fab18d2ceb7e29b71a5b5788e31bdb7c632d2c
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-04-26 11:46:28 +0200

    ex_143 execution

---
+++ wb_chapter6/exercise143.py
@@ -0,0 +1,38 @@
+##
+# Read 2 strings from user, determine if they are anagrams,
+# display the result
+#
+
+## Count the occurencies of each character of a string
+# @param s a string
+# @return a dictionary 
+#
+def countOccurencies(s):
+    # Create a dictionary
+    counter = {}
+    # For each char of string, add it as key in dictionary
+    # if not already stored and add 1 to its number of occurrencies
+    for char in s:
+        if char in counter:
+            counter[char] += 1
+        else:
+            counter[char] = 1
+    return counter
+
+# Read 2 strings from user 
+def main():
+    string1 = input("Please, enter the first string: ")
+    string2 = input("Please, enter the second string: ")
+    # Create 2 dictionaries with all the occurencies for
+    # each unique character
+    counter1 = countOccurencies(string1)
+    counter2 = countOccurencies(string2)
+    # Check if are anagrams and display the result
+    if counter1 == counter2:
+        print("%s and %s are anagrams" % (string1, string2))
+    else:
+        print("%s and %s aren't anagrams" % (string1, string2))
+
+# Call the main function
+if __name__ == "__main__":
+    main()
