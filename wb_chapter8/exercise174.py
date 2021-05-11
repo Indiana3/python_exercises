126ad44afc1a7d84d56fa57aa3ecd3a6645ad4ba
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-05-11 18:38:26 +0200

    ex_174 execution

---
+++ wb_chapter8/exercise174.py
@@ -0,0 +1,25 @@
+## Compute the greatest common divisor of 2 positive integers
+# @param a the first positive integer
+# @param b the second positive integer
+# @return the greatest common divisor
+#
+def greatestCommonDivisor(a, b):
+    # Base case
+    if b == 0:
+        return a
+    # Recursive case
+    c = a % b
+    print(a, b, c)
+    return greatestCommonDivisor(b, c)
+
+# Read 2 positive integers from user
+# Display the greatest common divisor
+def main():
+    a = int(input("Please, enter a positive integer: "))
+    b = int(input("Please, enter another positive integer: "))
+    divisor = greatestCommonDivisor(a, b)
+    print("The greatest common divisor of %d and %d is %d" % (a, b, divisor))
+
+# Call the main function
+if __name__ == "__main__":
+    main()
