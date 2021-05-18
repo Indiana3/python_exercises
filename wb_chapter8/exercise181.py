d59ec5220d5d4d6499d288b0ad5f36e320a2809f
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-05-18 19:46:22 +0200

    start again

--- wb_chapter8/exercise181.py
+++ wb_chapter8/exercise181.py
@@ -5,29 +5,33 @@
 # @return True if the value can be reached using the defined
 # number of coins; False otherwise
 #
-def possibleChange(dollars, coins):
+def possibleChange(dollars, coins, dollars_subtracted = 0):
     # Coins values
-    PENNY = 0.01
-    NICKEL = 0.05
-    DIME = 0.10
-    QUARTER = 0.25
+    coins_values = [0.01, 0.05, 0.10, 0.25]
+    # Remember the original values of dollars and coins
     # Base cases
     # No money and no coins return True
     if dollars == 0 and coins == 0:
         return True
-    # No money and coins left return False
-    if dollars <= 0 and coins > 0:
-        return False
-    # Money and no coins return False
-    if dollars > 0 and coins == 0:
-        return False
-    # Recursive case
-    return possibleChange(dollars-PENNY, coins-1) or \
-        possibleChange(dollars-NICKEL, coins-1) or \
-        possibleChange(dollars-DIME, coins-1) or \
-        possibleChange(dollars-QUARTER, coins-1)
+    # Either no money and coins or
+    # money and no coins return False
+    # Try with the following value in the list
+    if dollars == 0 or coins == 0:
+        coins_values.pop(0)
+        dollars = dollars + dollars_subtracted
+        dollars_subtracted = 0
+        if len(coins_values) == 0:
+            return False
+    # Recursive cases
+    # Try with the first element in the list
+    coin_value = coins_values[0]
+    # Store the money you subtract in a variable
+    dollars_subtracted += coin_value
+    return possibleChange(dollars-coin_value, coins-1)
 
 
-print(possibleChange(1, 5))
+print(possibleChange(0.50, 2))
+
+
         
     
