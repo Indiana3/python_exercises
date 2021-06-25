8d867ec04874e14cc47af45e5a2c9e5e28704624
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-06-25 18:22:10 +0200

    add documentation

--- tp_chapter14/exercise3.py
+++ tp_chapter14/exercise3.py
@@ -1,13 +1,30 @@
+##
+# Find files with same extension .txt in tp_chapter14 directory
+# and check if their content is the same or not
+#
 import os
 import hashlib
 
+## Find files with same extension in a directory and its subdirectories
+# @param dir the root directory
+# @param ext a string with file extension
+# @param same_extension an empty list where store files with same extension
+# @return same_extension
+#
 def sameExtention(dir=os.getcwd(), ext=".txt", same_extension = []):
+    # Use the number of characters of extension as index
     index = len(ext)
+    # For each file/directory in the root
     for name in os.listdir(dir):
+        # Get its absolute path
         path = os.path.join(dir, name)
+        # Check if it adresses to a file or a dir
         if os.path.isfile(path):
+            #Check if the extension of the file is equal to the given extension
             if path[-index:] == ext:
+                # Add it to the list
                 same_extension.append(path)
+        # If not a fill, call the function recursively with the new path
         else:
             sameExtention(path)
     return same_extension
